3a78b0fc47729222b6e046648c7fbb809e224214
XiaoliChan <2209553467@qq.com>
2023-08-30 02:20:08 +0800

    [ssh] Add tips
    Signed-off-by: XiaoliChan <2209553467@qq.com>

--- cme/protocols/ssh/proto_args.py
+++ cme/protocols/ssh/proto_args.py
@@ -4,7 +4,7 @@ def proto_args(parser, std_parser, module_parser):
     ssh_parser.add_argument("--port", type=int, default=22, help="SSH port (default: 22)")
     ssh_parser.add_argument("--ssh-timeout", help="SSH connection timeout, default is %(default)s secondes", type=int, default=5)
     ssh_parser.add_argument("--sudo-check", action="store_true", help="Check user privilege with sudo")
-    ssh_parser.add_argument("--sudo-check-method", choices={"sudo-stdin", "mkfifo"}, default="sudo-stdin", help="method to do with sudo check, default is '%(default)s'")
+    ssh_parser.add_argument("--sudo-check-method", choices={"sudo-stdin", "mkfifo"}, default="sudo-stdin", help="method to do with sudo check, default is '%(default)s (mkfifo is non-stable, probably you need to execute once again if it failed)'")
     ssh_parser.add_argument("--get-output-tries", help="Number of times with sudo command tries to get results, default is %(default)s", type=int, default=5)
 
     cgroup = ssh_parser.add_argument_group("Command Execution", "Options for executing commands")
